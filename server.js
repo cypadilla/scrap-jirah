59a577b34c81786e7553c6a7416dc2426c200ed7
Cristian Padilla <cristian.padilla@dipzo.net>
2024-12-12 20:31:59 -0500

    modify server to use chromium

--- server.js
+++ server.js
@@ -5,30 +5,24 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/scrape', async (req, res) => {
     try {
-        // Configuración necesaria para Puppeteer en entornos en la nube
         const browser = await puppeteer.launch({
-            headless: true, // Ejecuta en modo headless
-            args: ['--no-sandbox', '--disable-setuid-sandbox'], // Requerido en entornos como Render
+            headless: 'new',
+            executablePath: '/usr/bin/google-chrome', // Ruta donde está instalado Chrome
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
         });
-
         const page = await browser.newPage();
         await page.goto('https://web.bascbogota.com/node/5', { waitUntil: 'networkidle2' });
 
-        // Extrae el contenido de la página
         const content = await page.content();
+        res.send(content);
 
-        // Cierra el navegador
         await browser.close();
-
-        // Devuelve el contenido HTML
-        res.status(200).send(content);
     } catch (error) {
         console.error('Error durante el scraping:', error);
-        res.status(500).send({ error: 'Error al realizar el scraping.' });
+        res.status(500).json({ error: 'Error al realizar el scraping.' });
     }
 });
 
-// Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Servidor Puppeteer corriendo en http://localhost:${PORT}`);
 });
